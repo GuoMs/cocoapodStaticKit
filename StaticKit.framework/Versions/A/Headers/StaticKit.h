5453a3dd74524a05f598ac1bb3d0851ccf9cbcb5
郭明山 <753977066@qq.com>
2018-07-04 09:23:37 +0800

    第一次提交

---
+++ StaticKit.framework/Versions/A/Headers/StaticKit.h
@@ -0,0 +1,16 @@
+//
+//  StaticKit.h
+//  StaticKit
+//
+//  Created by GuoMS on 2018/6/24.
+//  Copyright © 2018年 GuoMs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface StaticKit : UIViewController
+
+
+    
+@end
